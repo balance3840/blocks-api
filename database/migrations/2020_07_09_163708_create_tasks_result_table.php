7fa24618ae32e2eed580e8f0d84f671319551cb3
Ramiro Estrella <ramiro.estrellac@gmail.com>
2020-07-09 18:43:59 +0200

    Creating table migrations

---
+++ database/migrations/2020_07_09_163708_create_tasks_result_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksResultTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks_result', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+		    $table->string('video');
+            $table->string('comment');
+            $table->foreignId('comment_user')->constrained('users');
+            $table->text('code');
+            $table->dateTime('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks_result');
+    }
+}
