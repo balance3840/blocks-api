3e24193a9ab26ba402419940bb14e2eecc321ade
Ramiro Estrella <raest@gramjuhl.com>
2021-07-05 18:56:29 +0200

    changes

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 docker-compose up -d --build --force-recreate
+# Copiando variables de entorno
 cp .env.example .env
-docker exec -it blocks-php composer install
-docker exec -it blocks-php php artisan key:generate
-docker exec -it blocks-db mysql -u root -pabc123 blocks < blocks.sql
+echo "Instalando dependencias de Composer"
+docker exec blocks-php composer install
+echo "Generando clave de artisan"
+docker exec blocks-php php artisan key:generate
+echo "Importando la base de datos"
+docker exec blocks-db mysql -u root -pabc123 blocks < blocks.sql
